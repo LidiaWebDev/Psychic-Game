a62a7cc5355fe58017cb4e4b649b8986019ae233
LidiaWebDev <lidia@fishisfast.com>
2018-10-19 22:47:13 -0700

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,56 @@
 
     
+    // Variable for the game
     var letters = ["a", "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
                     "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 6;
+    var guessesSoFar =[]; 
+    var showletters= null; 
+    var userChoice =[];
     var computerChoice = letters[Math.floor(Math.random()*letters.length)];
-        console.log(computerChoice);
-    var guessesleft = 6;
-    var wins = 5;
-    var losses = 5;
-    var results;
 
+    // A set of tasks for the computer behaviour in the game
+
+    document.getElementById ("keyPress").onkeypress =function (event){
+    var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+    var computerChoice = letters[Math.floor(Math.random()*letters.length)];
+    guessesSoFar.push(userChoice);
+    console.log(computerChoice); 
+    }
     
-    document.onkeyup = function(){
-        var userChoice = String.fromCharCode(event.keyCode). 
-        toLowerCase();
-        console.log(userChoice);
-
-
-     if (userChoice ===  computerChoice){
+    
+    if (userChoice ===  computerChoice){
          wins++;
+         alert("CONGRATULATIONS!You gessed it right!"
+               + wins + "Wins "); 
+         document.getElementById("wins").innerHTML = "Wins: " + wins;
+         guessesLeft=6;
+         guessesSoFar.length = 0;
+         startAgain();
+         console.log("you won!"); 
      }
-     else{
-         guessesLeft  --;
-     }
-     if (guesses == 0){
-         losses++
-     }
-     }
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    document.getElementById("losses").innerHTML = "losses: " + losses;
-    document.getElementById("guessesLeft").innerHTML = "Guessesleft: " + guesses;
+     else if (guessesLeft == 0) { 
+         losses++;
+         alert("OOPS SORRY!! TRY AGAIN!");
+         document.getElementById("lose").innerHTML = "Losses: " + losses;
+         guessesLeft=6;
+         guessesSoFar.length = 0;
+         startAgain();
+         console.log("try again!")
+    }
 
+      else if (userChoice !== computerChoice) {
+          guessesLeft --;
+         
+      }
+    
+        
+         
+     
+     
+     
+    
     
     
